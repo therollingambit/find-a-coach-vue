c3c51838d40630d6a00b85e62fa1c021f1d0499b
therollingambit <therollingambit@gmail.com>
2022-06-22 00:40:27 +0800

    add Vuex and Coach data

--- src/views/coaches/CoachesList.vue
+++ src/views/coaches/CoachesList.vue
@@ -7,8 +7,24 @@
       <router-link to="/register">Register as a Coach</router-link>
     </div>
 
-    <ul>
-      List of coaches
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters["coaches/coaches"];
+    },
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
+</script>
