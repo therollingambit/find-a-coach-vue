bd77d71bbee693bd074c275b2e5e8fbb574bee69
therollingambit <therollingambit@gmail.com>
2022-06-21 22:52:33 +0800

    register routes

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      redirect: "/coaches"
+    },
+    {
+      path: "/coaches",
+      component: null,
+    },
+    {
+      path: "/coaches/:id",
+      component: null,
+      children: [
+        { path: 'contact', component: null }
+      ]
+    },
+    {
+      path: "/register",
+      component: null,
+    },
+    {
+      path: "/requests",
+      component: null,
+    },
+    {
+      path: "/:notFound(.*)",
+      component: null,
+    },
+  ],
+});
+
+export default router;
