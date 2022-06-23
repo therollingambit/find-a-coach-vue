1cb5f3eb1629036a144be3b6b2c77b64c9f2fce4
therollingambit <therollingambit@gmail.com>
2022-06-23 15:43:16 +0800

    store message requests with Vuex

--- src/views/requests/ContactCoach.vue
+++ src/views/requests/ContactCoach.vue
@@ -41,7 +41,13 @@ export default {
         return;
       }
 
-      
+      this.$store.dispatch("requests/contactCoach", {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.params.id,
+      });
+
+      this.$router.replace("/coaches");
     },
   },
 };
